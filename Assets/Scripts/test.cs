65f8378e4a38a6e0d5c3d9f8d562dbf20bd5078e
carolinejjia <jia.caroline@gmail.com>
2025-09-12 18:39:31 -0400

    Made a script

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public float speed;
+    [SerializeField] private float speed2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
