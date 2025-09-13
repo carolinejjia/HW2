01b81afc47b5f90e1417cf93ed8f39c6385fdefe
carolinejjia <jia.caroline@gmail.com>
2025-09-12 22:13:55 -0400

    hw2 submission

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    // Update is called once per frame
+    //private Vector2 movementValue;
+    //private float lookValue;
+
+    private void Awaek()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    /*public void OnMove(InputValue value)
+    {
+        movementValue = value.Get<Vector2>() * speed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>().x * rotationSpeed
+    }
+    */
     void Update()
     {
+
+
+        //transform.Translate(
+        //    movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
+        //transform.Rotate(0, lookValue * Time.deltaTime, 0);
+
         float mouseX = Input.GetAxis("Mouse X");
         transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
